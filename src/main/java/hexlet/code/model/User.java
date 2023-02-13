a726984409966e19bc40fb482d84c7307672adbc
Alena Komarnitskaya <mixinny1@gmail.com>
2023-02-14 04:47:53 +0500

    step 3 with PasswordEncoder error

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,53 @@
+package hexlet.code.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.ui.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @JsonIgnore
+    private String password;
+
+    @CreationTimestamp
+    @Temporal(TIMESTAMP)
+    private Date createdAt;
+
+    public User(final Long id) {
+        this.id = id;
+    }
+
+
+}
